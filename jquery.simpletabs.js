ddd695664abca851eeb971a9a811ed746c56ea3e
Shengying Pan <span@teebie.com>
2012-08-16 13:13:58 -0400

    Bug fixes, added options -> hide show functions. Need to fix the images though.

--- jquery.simpletabs.js
+++ jquery.simpletabs.js
@@ -1,34 +1,85 @@
-(function($){
-    $.fn.simpleTabs = function(){
+(function($) {
+    $.fn.simpleTabs = function(options) {
+        var isHidden = false;
+
+        this.addClass("tabs-container");
+
         var ul = this.find("ul:first");
         var lis = this.find("ul:first li");
         var divs = new Array();
 
         $(ul).addClass("tabs-navigation");
         $(lis).addClass("tabs-nav-tab");
-        
-        
-        for(var i = 0; i < lis.length; i++){
+
+        var counter = 0;
+        for(var i = 0; i < lis.length; i++) {
             var id = $(lis[i]).find("i").attr("data-link");
             $(id).addClass("tabs-content");
-            
-            if (i == 0) {
+
+            if(i == 0) {
                 $(lis[i]).addClass("tabs-tab-active");
             }
             else {
                 $(lis[i]).addClass("tabs-tab-inactive");
                 $(id).hide();
             }
-            divs.push($(id)[0]);
+
+            if(typeof $(id)[0] !== "undefined") {
+                counter++;
+                divs.push($(id)[0]);
+            }
+            else {
+                //if a tab has no corresponding div, hide it
+                $(lis[i]).hide();
+            }
         }
 
-        $(lis).click(function(){
+        if(counter == 0) {
+            //If no content is present, hide everything, safe keeper
+            this.hide();
+            return;
+        }
+
+        $(lis).click(function() {
             $(lis).removeClass("tabs-tab-active").addClass("tabs-tab-inactive");
             $(this).removeClass("tabs-tab-inactive").addClass("tabs-tab-active");
             var id = $(this).find("i").attr("data-link");
             $(divs).hide();
-            $(id).show();
+
+            if(!isHidden) {
+                $(id).show(); //only show a tab if not in hidden mode
+            }
             return false;
         });
+
+        //Deal with extra options
+        if(options != null) {
+            //Set default if not present
+            options.enableHideShow = options.enableHideShow == null ? false : options.enableHideShow;
+
+            if(options.enableHideShow) {
+                var li = $('<li class="tabs-nav-tab-hs"></li>').appendTo(ul);
+
+                $('<span class="tabs-nav-hs-span">Hide</span><img class="tabs-nav-hs-img tabs-nav-hs-img-min" />').click(function() {
+                    var li = ul.find(".tabs-nav-tab-hs");
+                    var hideShowSpan = $(".tabs-nav-hs-span", li);
+                    var hideShowImg = $(".tabs-nav-hs-img", li);
+
+                    var bIsHidden = (hideShowSpan.html() == "Show");
+                    if(bIsHidden) {
+                        isHidden = false;
+                        $(ul.find(".tabs-tab-active i").attr("data-link")).show();
+                        hideShowSpan.html("Hide");
+                        hideShowImg.removeClass("tabs-nav-hs-img-max").addClass("tabs-nav-hs-img-min");
+                    }
+                    else {
+                        isHidden = true;
+                        $(divs).hide();
+                        hideShowSpan.html("Show");
+                        hideShowImg.removeClass("tabs-nav-hs-img-min").addClass("tabs-nav-hs-img-max");
+                    }
+                }).appendTo(li);
+            }
+        }
     }
 })(jQuery);
